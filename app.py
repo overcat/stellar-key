d56ff533f90a962386a7f80bf1fc1383429b0bc8
Jun Luo <4catcode@gmail.com>
2020-03-06 09:22:00 +0800

    cors.

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, response
 from stellar_sdk import Keypair
 
 app = Flask(__name__)
+
+def add_cors_headers(response):
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    if request.method == 'OPTIONS':
+        response.headers['Access-Control-Allow-Methods'] = 'DELETE, GET, POST, PUT'
+        headers = request.headers.get('Access-Control-Request-Headers')
+        if headers:
+            response.headers['Access-Control-Allow-Headers'] = headers
+    return response
+
+app.after_request(add_cors_headers)
 
 @app.route("/")
 def main():
